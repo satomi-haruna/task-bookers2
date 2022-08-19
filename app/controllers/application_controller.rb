adf221616499e96b4b029c6d0d2846bb54272ce8
satomi-haruna <satomi.haruna1124@gmail.com>
2022-08-19 22:55:54 +0000

    [Add] 4-25 SignUpのname追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  # ユーザ登録・ログイン認証の前にメソッドか実行される
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  # nameのデータを保存できるよう、許可を与える
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,keys:[:name])
+  end
+
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
   config.hosts.clear
+  # エラー回避のために記述
   config.active_job.queue_adapter = :inline
 end
